536ee04c21d7f778041f9f5561b67b1e55d29d88
Crime000 <amgd.skellington@gmail.com>
2024-06-11 21:01:47 +0100

    Movimiento y Camara
    Completado el Movimiento del jugador y el de la cámara, además de setear los action maps para la mayoría de interacciones.

--- Assets/#Scripts/MovimientoJugador.cs
+++ Assets/#Scripts/MovimientoJugador.cs
@@ -2,30 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MovimientoJugador : MonoBehaviour
 {
     Rigidbody rb;
     PlayerInput playerInput;
+    public float velocidad;
+    private Quaternion rotacionActual;
+    public Transform cam;
+    public InputActionReference Recoger, Abrir, Inventario;
 
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main.transform;
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
         playerInput = GetComponent<PlayerInput>();
+        rotacionActual = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector2 leido = playerInput.actions["Movimiento"].ReadValue<Vector2>();
-        rb.velocity = new Vector3(leido.x, 0, leido.y);
+        Vector3 Direction = new Vector3(leido.x, 0, leido.y);
+        Direction = cam.transform.forward * leido.y + cam.transform.right * leido.x;
+        Direction.y = 0f;
+        rb.velocity = Direction * velocidad;
     }
 
-    public void Movimiento(InputAction.CallbackContext context)
+    private void OnEnable()
     {
-        Vector2 accion = context.action.ReadValue<Vector2>();
-        rb.velocity = accion;
+        Recoger.action.started += recoger;
+        Abrir.action.started += abrir;
+        Inventario.action.started += inventario;
+    }
+
+    private void OnDisable()
+    {
+        Recoger.action.started -= recoger;
+        Abrir.action.started -= abrir;
+        Inventario.action.started -= inventario;
+    }
+
+    public void recoger(InputAction.CallbackContext context)
+    {
+        Debug.Log("Recoger");
+    }
+
+    public void abrir(InputAction.CallbackContext context)
+    {
+        Debug.Log("Abrir");
+    }
+
+    public void inventario(InputAction.CallbackContext context)
+    {
+        Debug.Log("Inventario");
     }
 }
